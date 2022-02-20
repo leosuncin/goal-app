dc66a4167451cf91eab755936f1dfd0553597478
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2022-02-19 20:36:13 -0600

    build: install unplugin-icons

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack(config) {
+    config.plugins.push(
+      require('unplugin-icons/webpack')({
+        compiler: 'jsx',
+        jsx: 'react',
+      })
+    );
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
