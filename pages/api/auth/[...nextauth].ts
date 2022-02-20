b34822cbb7334230bffd04a4925657ae6525ea83
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2022-02-20 17:46:04 -0600

    feat: enable credentials provider with MongoDB

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,8 +1,16 @@
 import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
-import NextAuth from 'next-auth';
+import { verify } from '@node-rs/bcrypt';
+import NextAuth, { type User } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
 import EmailProvider from 'next-auth/providers/email';
 
 import { getMongoClient } from '../../../lib/mongodb';
+
+declare module 'next-auth' {
+  interface User {
+    password?: string;
+  }
+}
 
 export default NextAuth({
   adapter: MongoDBAdapter(getMongoClient()),
@@ -19,7 +27,45 @@ export default NextAuth({
         ignoreTLS: true,
       },
     }),
+    CredentialsProvider({
+      name: 'Account',
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'email',
+          placeholder: 'Enter your email',
+        },
+        password: {
+          label: 'Password',
+          type: 'password',
+          placeholder: 'Enter password',
+        },
+      },
+      async authorize(credentials) {
+        if (!credentials) return null;
+
+        const client = await getMongoClient();
+        const users = client.db().collection<User>('users');
+        const user = await users.findOne({ email: credentials.email });
+
+        if (!user) return null;
+
+        if (
+          typeof user.password === 'string' &&
+          !(await verify(credentials.password, user.password))
+        ) {
+          return null;
+        } else {
+          delete user.password;
+        }
+
+        return user;
+      },
+    }),
     // ...add more providers here
   ],
   debug: process.env.NODE_ENV === 'development',
+  session: {
+    strategy: 'jwt',
+  },
 });
