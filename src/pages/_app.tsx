a1138f30ffce7d69057b9c918a91b89dcfe64955
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2022-09-29 11:35:19 -0600

    fix: amend changes caused next-redux-wrapper update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,24 @@
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
+import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import '~app/styles/globals.css';
 import { wrapper } from '~app/lib/store';
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+function MyApp({ Component, ...rest }: AppProps) {
+  const { store, props } = wrapper.useWrappedStore(rest);
+  const { session } = props.pageProps;
+
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
-      <ToastContainer />
+      <Provider store={store}>
+        <Component {...props.pageProps} />
+        <ToastContainer />
+      </Provider>
     </SessionProvider>
   );
 }
 
-export default wrapper.withRedux(MyApp);
+export default MyApp;
