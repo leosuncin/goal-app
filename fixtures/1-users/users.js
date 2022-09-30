184097b6ac9770994f4b1c9bc1d6c13057f3f051
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2022-09-29 21:03:22 -0600

    ci(github): run e2e test with cypress

---
+++ fixtures/1-users/users.js
@@ -0,0 +1,28 @@
+const { faker } = require('@faker-js/faker');
+const { getObjectId } = require('mongo-seeding');
+
+/**
+ * @type {Array<import('next-auth').User>}
+ */
+const users = [
+  {
+    _id: getObjectId('john-doe'),
+    id: getObjectId('john-doe').toHexString(),
+    name: 'John Doe',
+    email: 'john-doe@example.com',
+    /* cspell:disable-next-line */
+    password: '$2y$12$c7ne7Aqsx0lGgL/P4HYwRu1zybpsDQ7zpzZXbLT9g3Pafwt5eiPDe',
+    image: faker.image.avatar(),
+  },
+  {
+    _id: getObjectId('user01'),
+    id: getObjectId('user01').toHexString(),
+    name: faker.name.fullName(),
+    email: faker.internet.exampleEmail(),
+    /* cspell:disable-next-line */
+    password: '$2y$12$5Og.21J6jSUkCrlTnrGM3OfIZyAVpwpmVNNxWCXj0t/.W6q7cihW.',
+    image: faker.image.avatar(),
+  },
+];
+
+module.exports = users;
