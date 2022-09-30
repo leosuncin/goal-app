23901e3be907578599bc883d9c47d6855688255e
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2022-09-29 21:03:22 -0600

    ci(github): run e2e test with cypress

---
+++ fixtures/2-goals/goals.js
@@ -0,0 +1,32 @@
+const { getObjectId } = require('mongo-seeding');
+
+/**
+ * @typedef {object} Goal
+ * @property {ObjectId} _id
+ * @property {String} text
+ * @property {ObjectId} author
+ * @property {Date} createdAt
+ */
+
+/**
+ * @type {Array<Goal>}
+ */
+const goals = [
+  {
+    _id: getObjectId('goal01'),
+    text: 'Make a sandwich',
+    author: getObjectId('john-doe'),
+  },
+  {
+    _id: getObjectId('goal02'),
+    text: 'Make a salad',
+    author: getObjectId('john-doe'),
+  },
+  {
+    _id: getObjectId('goal03'),
+    text: 'Buy milk',
+    author: getObjectId('john-doe'),
+  },
+];
+
+module.exports = goals;
