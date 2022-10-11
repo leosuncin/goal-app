360e293c3ca824f4e16f66a910902a2b091452cb
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2022-10-11 14:32:29 -0600

    chore: change fixtures
    Set the email to a static value
    Add more fixtures to goals

--- fixtures/1-users/users.js
+++ fixtures/1-users/users.js
@@ -18,7 +18,7 @@ const users = [
     _id: getObjectId('user01'),
     id: getObjectId('user01').toHexString(),
     name: faker.name.fullName(),
-    email: faker.internet.exampleEmail(),
+    email: 'imprudent0869@example.com',
     /* cspell:disable-next-line */
     password: '$2y$12$5Og.21J6jSUkCrlTnrGM3OfIZyAVpwpmVNNxWCXj0t/.W6q7cihW.',
     image: faker.image.avatar(),
